f4ea0a195cf590802cd3f5b806628213a275a309
BennyAbhishikth <benny596@sasi.ac.in>
2024-01-10 11:00:21 +0530

    Database

--- Database/connect.inc.php
+++ Database/connect.inc.php
@@ -1,8 +1,8 @@
 <?php
-	$mysql_host='localhost';
-	$mysql_user='root';
+	$mysql_host='';
+	$mysql_user='';
 	$mysql_pass='';
-	$mysql_db='infodb';
+	$mysql_db='';
 	$con=mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
 	if(!$con )	
 	{
